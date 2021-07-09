0ad5c09136be5999a82c835cd35ced97a60f748e
camha91 <crystalhanguyen@gmail.com>
2021-07-09 15:37:27 -0700

    Use animation to drop the task

--- src/pages/DemoDragDrop/DemoDragDrop.js
+++ src/pages/DemoDragDrop/DemoDragDrop.js
@@ -1,4 +1,6 @@
 import React, { useState, useRef } from "react";
+import "./DemoDragDrop.css";
+import { useSpring, animated } from "react-spring";
 
 const defaultValue = [
     { id: 1, taskName: "Task 1" },
@@ -11,6 +13,15 @@ const defaultValue = [
 export default function DemoDragDrop() {
     const [taskList, setTaskList] = useState(defaultValue);
     const tagDrag = useRef({});
+    const tagDragEnter = useRef({});
+
+    // Animation
+    const [propsSpring, set, stop] = useSpring(() => ({
+        from: { bottom: -25 },
+        to: { bottom: 0 },
+        config: { duration: 250 },
+        reset: true,
+    }));
 
     const handleDragOver = (e) => {
         // console.log("targetOver", e.target);
@@ -37,6 +48,10 @@ export default function DemoDragDrop() {
         console.log("targertOver", taskDragEnter);
         console.log("index", index);
 
+        // Save the value of the task that is passed by
+        set({ bottom: 0 });
+        tagDragEnter.current = { ...taskDragEnter };
+
         const taskListUpdate = [...taskList];
 
         // Get index of the task that is dragging
@@ -62,12 +77,52 @@ export default function DemoDragDrop() {
     };
 
     return (
-        <div className="container">
+        <div
+            className="container"
+            onDragOver={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+            }}
+            onDrop={(e) => {
+                tagDrag.current = {};
+                console.log("dragEnd");
+                setTaskList([...taskList]);
+            }}
+        >
             <div className="text-center display-4">Task list</div>
             <div className="row">
                 <div className="col-2"></div>
                 <div className="bg-dark p-5 col-4">
                     {taskList.map((task, index) => {
+                        const cssDragTag =
+                            task.id === tagDrag.current.id ? "dragTag" : "";
+
+                        if (task.id === tagDragEnter.current.id) {
+                            return (
+                                <animated.div
+                                    style={{
+                                        position: "relative",
+                                        bottom: propsSpring.bottom.to(
+                                            (numBottom) => `${numBottom}px`
+                                        ),
+                                    }}
+                                    onDragStart={(e) => {
+                                        handleDragStart(e, task, index);
+                                    }}
+                                    onDragEnter={(e) => {
+                                        handleDragEnter(e, task, index);
+                                    }}
+                                    onDragEnd={(e) => {
+                                        handleDragEnd(e);
+                                    }}
+                                    draggable="true"
+                                    key={index}
+                                    className={`bg-success text-white m-1 p-3 ${cssDragTag}`}
+                                >
+                                    {task.taskName}
+                                </animated.div>
+                            );
+                        }
                         return (
                             <div
                                 onDragStart={(e) => {
