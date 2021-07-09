7a917923bb1d13207b3b986ca9d7cdc8dfe3e40a
camha91 <crystalhanguyen@gmail.com>
2021-07-09 15:01:34 -0700

    Drag the task to different location on the task list

--- src/pages/DemoDragDrop/DemoDragDrop.js
+++ src/pages/DemoDragDrop/DemoDragDrop.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const defaultValue = [
     { id: 1, taskName: "Task 1" },
@@ -10,30 +10,60 @@ const defaultValue = [
 
 export default function DemoDragDrop() {
     const [taskList, setTaskList] = useState(defaultValue);
+    const tagDrag = useRef({});
 
     const handleDragOver = (e) => {
-        console.log("targetOver", e.target);
+        // console.log("targetOver", e.target);
     };
 
     const handleDragStart = (e, task, index) => {
         console.log("tag", e.target);
         console.log("task", task);
         console.log("index", index);
+        // Save the value of the task that is dragging
+        tagDrag.current = task;
     };
 
     const handleDragEnd = (e) => {
-        console.log("dragEnd", e.target);
+        // console.log("dragEnd", e.target);
     };
 
     const handleDrop = (e) => {
-        console.log("drop", e.target);
+        // console.log("drop", e.target);
+    };
+
+    const handleDragEnter = (e, taskDragEnter, index) => {
+        console.log("dragEnterTag", e.target);
+        console.log("targertOver", taskDragEnter);
+        console.log("index", index);
+
+        const taskListUpdate = [...taskList];
+
+        // Get index of the task that is dragging
+        const indexDragTag = taskListUpdate.findIndex(
+            (task) => task.id === tagDrag.current.id
+        );
+
+        // Get index of the replaced tag
+        const indexDragEnter = taskListUpdate.findIndex(
+            (task) => task.id === taskDragEnter.id
+        );
+
+        // Var that contain dragging task value
+        const temp = taskListUpdate[indexDragTag];
+
+        // Get the coordinate of the task that is dragging = the replaced task
+        taskListUpdate[indexDragTag] = taskListUpdate[indexDragEnter];
+
+        // Get the coordinate of the replaced task = the dragging task
+        taskListUpdate[indexDragEnter] = temp;
+
+        setTaskList(taskListUpdate);
     };
 
     return (
         <div className="container">
-            <div className="text-center display-4" onDragOver={handleDragOver}>
-                Task list
-            </div>
+            <div className="text-center display-4">Task list</div>
             <div className="row">
                 <div className="col-2"></div>
                 <div className="bg-dark p-5 col-4">
@@ -43,7 +73,9 @@ export default function DemoDragDrop() {
                                 onDragStart={(e) => {
                                     handleDragStart(e, task, index);
                                 }}
-                                onDragEnter={handleDragOver}
+                                onDragEnter={(e) =>
+                                    handleDragEnter(e, task, index)
+                                }
                                 onDragEnd={(e) => {
                                     handleDragEnd(e);
                                 }}
@@ -59,13 +91,7 @@ export default function DemoDragDrop() {
                 <div
                     className="col-2 bg-primary"
                     style={{ height: 500 }}
-                    onDragOver={(e) => {
-                        e.stopPropagation();
-                        e.preventDefault();
-                    }}
-                    onDrop={(e) => {
-                        handleDrop(e);
-                    }}
+                    onDragOver={(e) => {}}
                 >
                     dsadsasda
                 </div>
